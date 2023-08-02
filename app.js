181959276ca258feec02156b468468714cc7390b
Favio Valero <favio102@protonmail.com>
2023-08-02 06:27:33 -0700

    react added for fecth portfolio list

--- app.js
+++ app.js
@@ -52,79 +52,41 @@ function fetchContactContent() {
 // Call the function to fetch the contact content when the page loads
 document.addEventListener('DOMContentLoaded', fetchContactContent);
 
+// Load data from data.json
+fetch('data.json')
+  .then(response => response.json())
+  .then(data => {
+    const projectItems = data.projects;
+    const sectionCenter = document.getElementById("portfolioList");
+    displayProjectItems(projectItems, sectionCenter);
+  })
+  .catch(error => console.error('Error fetching data:', error));
 
-
-
-
-
-
-// Function to fetch and interpolate data from data.json
-function fetchAndInterpolateData() {
-  fetch('data.json')
-      .then(response => response.json())
-      .then(data => {
-          const portfolioList = document.getElementById('portfolioList');
-          data.projects.forEach(project => {
-              const portfolioItem = document.createElement('div');
-              portfolioItem.classList.add('portfolio-item');
-
-              const image = document.createElement('div');
-              image.classList.add('image');
-              const img = document.createElement('img');
-              img.setAttribute('src', project.img);
-              img.setAttribute('alt', project.title);
-              image.appendChild(img);
-
-              const hoverItems = document.createElement('div');
-              hoverItems.classList.add('hover-items');
-
-              const h3 = document.createElement('h3');
-              h3.setAttribute('id', 'title');
-
-              const listItem1 = document.createElement('ul');
-              listItem1.textContent = project['sub-title'];
-
-              const listItem2 = document.createElement('ul');
-              listItem2.textContent = project.title;
-
-              h3.appendChild(listItem1);
-              h3.appendChild(listItem2);
-
-              const icons = document.createElement('div');
-              icons.classList.add('icons');
-
-              const link1 = document.createElement('a');
-              link1.setAttribute('href', project['url-source']);
-              link1.setAttribute('target', '_blank');
-              link1.classList.add('icon');
-              const icon1 = document.createElement('i');
-              icon1.classList.add('fab', 'fa-github');
-              link1.appendChild(icon1);
-
-              const link2 = document.createElement('a');
-              link2.setAttribute('href', project['url-page']);
-              link2.setAttribute('target', '_blank');
-              link2.classList.add('icon');
-              const icon2 = document.createElement('i');
-              icon2.classList.add('fas', 'fa-globe-africa');
-              link2.appendChild(icon2);
-
-              icons.appendChild(link1);
-              icons.appendChild(link2);
-
-              hoverItems.appendChild(h3);
-              hoverItems.appendChild(icons);
-
-              portfolioItem.appendChild(image);
-              portfolioItem.appendChild(hoverItems);
-
-              portfolioList.appendChild(portfolioItem);
-          });
-      })
-      .catch(error => {
-          console.error('Error fetching and interpolating data:', error);
-      });
+function displayProjectItems(projectItems, sectionCenter) {
+  let displayProject = projectItems.map(function (item) {
+    return `
+      <div class="portfolio-item">
+        <div class="image">
+          <img src=${item.img} alt=${item.title}>
+        </div>
+        <div class="hover-items">
+          <h3 id="title">
+            <li>
+              <ul>${item['sub-title']}</ul>
+              <ul>${item.title}</ul>
+            </li>
+          </h3>
+          <div class="icons">
+            <a href=${item['url-source']} target="_blank" class="icon">
+            <i class="fab fa-github"></i>
+            </a>
+            <a href=${item['url-page']} class="icon" target="_blank">
+            <i class="fab fa-globe"></i>
+            </a>
+          </div>
+        </div>
+      </div>`;
+  });
+  displayProject = displayProject.join("");
+  sectionCenter.innerHTML = displayProject;
 }
-
-// Call the function to fetch and interpolate data when the page loads
-document.addEventListener('DOMContentLoaded', fetchAndInterpolateData);
